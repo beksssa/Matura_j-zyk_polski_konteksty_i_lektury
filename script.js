dd91a889399033d4befaf4f3d2ae9b8a44859227
pszczolkamaja1 <adamm.matloka@gmail.com>
2026-05-27 22:38:05 +0200

    script.js

--- script.js
+++ script.js
@@ -156,26 +156,12 @@ characters: [
   },
   
   {
-    id: "hajmon",
-    name: "Hajmon",
-    description: "Symbol marazmu narodowego i uśpienia społeczeństwa polskiego.",
-    motifs: ["symbolizm", "motywnarodowy"]
-  },
-  
-  {
     id: "raskolnikow",
     name: "Raskolnikow",
     description: "Student przekonany o własnej wyjątkowości, który morderstwem próbuje udowodnić swoją teorię nadczłowieka.",
     motifs: ["motywsumienia"]
   },
-
-  {
-    id: "wokulski",
-    name: "Stanisław Wokulski",
-    description: "Zamożny kupiec, właściciel sklepu galanteryjnego. Zakochany w Izabeli Łęckiej, którą chce do siebie przekonać swoim majątkiem.",
-    motifs: ["motywmiłości"]
-  },
-
+  
   {
     id: "sonia",
     name: "Sonia Marmieładowa",
@@ -189,7 +175,119 @@ characters: [
     description: "Kuzyn i ryczerz króla Dunkana. Po usłyszeniu przepowiedni czarownic budzi się w nim żądza władzy. Podstępem morduje króla i sam przejmuje władzę. Nękany jest póćniej strachem i wątpliwościami.",
     motifs: ["fatum", "motywsumienia"]
   },
+
+  {
+    id: "lmakbet",
+    name: "Lady Makbet",
+    description: "Żona makbeta, która planuje zabójstwo Dunkana. Nasila żądzę władzy męża. Ostatecznie popada w szaleństwo.",
+    motifs: []
+  },
+
+  {
+    id: "artur",
+    name: "Artur",
+    description: "Młody student medycyny. Stara się przywrócić w domu dawny porządek.",
+    motifs: []
+  },
   
+  {
+    id: "edek",
+    name: "Edek",
+    description: "Niewykształcony prostak pozbawiony zasad, jednak uważany przez Stomila za zabawnego i prawdziwego. Wierzy, że jego siła fizyczna daje mu władzę.",
+    motifs: []
+  },
+
+  {
+    id: "winston",
+    name: "Winston Smith",
+    description: "Pracownik Departamentu Archiwów w Ministerstwie Prawdy. Sprzeciwia się reżimowi, prowadzi pamiętnik, ma kochankę, chce walczyć z systemem.",
+    motifs: []
+  },
+  
+  {
+    id: "brat",
+    name: "Wielki Brat",
+    description: "Tajemniczy przywódca Oceanii. Stoi on za całą inwigilacją i kontrolą społeczeństwa. Jednocześnie samo jego istnienie jest niepewne, może on równie dobrze być tylko symbolem używanym przez reżim.",
+    motifs: []
+  },
+  
+  {
+    id: "harpagon",
+    name: "Harpagon",
+    description: "Paryski mieszczanin i lichwiarz. Cechuje go wyjątkowe skąpstwo. Jego żądza gromadzenia pieniędzy zatruwa jego życie rodzinne.",
+    motifs: []
+  },
+
+  {
+    id: "wokulski",
+    name: "Stanisław Wokulski",
+    description: "Zamożny kupiec, właściciel sklepu galanteryjnego. Zakochany w Izabeli Łęckiej, którą chce do siebie przekonać swoim majątkiem.",
+    motifs: ["motywmiłości"]
+  },
+
+  {
+    id: "rzecki",
+    name: "Ignacy Rzecki",
+    description: "Przyjaciel Wokulskiego, pracuje w jego sklepie. Prowadzi pamiętnik. Próbuje zeswatać Wokulskuego z Stawską",
+    motifs: []
+  },
+
+  {
+    id: "lecka",
+    name: "Izabela Łęcka",
+    description: "Młoda arystokratka, której rodzina straciła majątek. Uważana za niezwykle piękną. Powierzchownie interesuje się sztuką i kulturą, tak naprawdę jest płytka i materialistyczna.",
+    motifs: []
+  },
+
+  {
+    id: "kmicic",
+    name: "Andrzej Kmicic",
+    description: "Młody szlachcic, ma ożenić się z Oleńką. Z początku skłonny do przemocy awanturnik, przechodzi przemianę pod nazwiskiem Babinicz. Staje w obronie ojczyzny i odzyskuje swój honor.",
+    motifs: []
+  },
+
+  {
+    id: "michal",
+    name: "Michał Wołodyjowski",
+    description: "Polski pułkownik. Uczciwy i sprawiedliwy, ceniący honor podan wszystko. Oddany walce przeciwko szwedom.",
+    motifs: []
+  },
+
+  {
+    id: "baryka",
+    name: "Cezary Baryka",
+    description: "Syn Carskiego urzędnika mieszkający w dzieciństwie w Baku. Po odzyskaniu przez Polskę niepodległości wraca do kraju. Jest świadkiem sytuacji społecznej i ekonomicznej młodego państwa, będąc również kuszony przez idee rewolucyjne.",
+    motifs: []
+  },
+
+  {
+    id: "starybaryka",
+    name: "Seweryn Baryka",
+    description: "Ojciec Cezarego, urzędnik Cesarski. Idealizuje Polskę, wpaja synowi ideę 'szklanych domów'. Umiera w drodze do ojczyzny.",
+    motifs: []
+  },
+
+  {
+    id: "jozek",
+    name: "Józio Kowalksi",
+    description: "Trzydziestoletni Józef, który zmienia się w szesnastolatka i wraca do szkoły. Podróżuje następnie przez stancję Młodziaków i wieś Miętusa. W każdym z tych miejsc spotyka się i konfrontuje role nadawane ludziom przez społeczeństwo",
+    motifs: []
+  },
+
+  {
+    id: "mietus",
+    name: "Miętus",
+    description: "W szkole przedstawiciel chłopców, czyli tych, którzy swoją dorosłość próbują udowodnić przez wulgarne zachowanie (coś do dokończenia jeszcze",
+    motifs: []
+  },
+
+  {
+    id: "rieux",
+    name: "Bernard Rieux",
+    description: "Lekarz mieszkający w Oranie. Od samego początku walczy z epidemią i całkowicie się temu oddaje.",
+    motifs: []
+  },
+
   ]  
 };   
 
